f8ca5644c842731a09381436f2c84fa662eae3e5
JauhienKaraliou <jauhien.karaliou@gmail.com>
2015-04-29 10:38:33 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+if (isset($argv[1])) {
+    echo 'hi ' . "\033[0;36m" . $argv[1] . "\033[0;37m!\n";
+} else {
+    echo 'hi,'."\033[0;35m Anonymous! \n\033[0;37m" . "\n";
+}
+do {
+    $handle = fopen("php://stdin", "r");
+    $line = fgets($handle);
+    $line=trim($line);
+    if ("exit\n" == $line) {
+        echo 'bue!' . "\n";
+        die();
+    } else {
+        echo ' everyone can say ' ."\033[0;36m\"".$line."\"\n\033[0;37m But what about to buy an elephant?\n";
+    }
+} while (1);
