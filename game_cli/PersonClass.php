62f3d065010c3287c521b1e7576d007c8c193270
jauhien <jauhien.karaliou@gmail.com>
2015-06-29 08:36:00 +0300

    hero moves properly

--- game_cli/PersonClass.php
+++ game_cli/PersonClass.php
@@ -1,5 +1,143 @@
 <?php
 
-class PersonClass {
+class Person
+{
 
+    protected $currentX;
+
+    protected $currentY;
+
+    protected $previousX;
+
+    protected $previousY;
+
+    protected $symbol;
+
+    protected $colour;
+
+    /**
+     * @return mixed
+     */
+    public function getCurrentX()
+    {
+        return $this->currentX;
+    }
+
+    /**
+     * @param mixed $currentX
+     */
+    public function setCurrentX($currentX)
+    {
+        $this->currentX = $currentX;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCurrentY()
+    {
+        return $this->currentY;
+    }
+
+    /**
+     * @param mixed $currentY
+     */
+    public function setCurrentY($currentY)
+    {
+        $this->currentY = $currentY;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPreviousX()
+    {
+        return $this->previousX;
+    }
+
+    /**
+     * @param mixed $previousX
+     */
+    public function setPreviousX($previousX)
+    {
+        $this->previousX = $previousX;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPreviousY()
+    {
+        return $this->previousY;
+    }
+
+    /**
+     * @param mixed $previousY
+     */
+    public function setPreviousY($previousY)
+    {
+        $this->previousY = $previousY;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSymbol()
+    {
+        return $this->symbol;
+    }
+
+    /**
+     * @param mixed $symbol
+     */
+    public function setSymbol($symbol)
+    {
+        $this->symbol = $symbol;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getColour()
+    {
+        return $this->colour;
+    }
+
+    /**
+     * @param mixed $colour
+     */
+    public function setColour($colour)
+    {
+        $this->colour = $colour;
+    }
+
+    public function moveUp()
+    {
+        $this->setPreviousState();
+        $this->setCurrentY($this->getCurrentY()-1);
+    }
+
+    public function moveDown()
+    {
+        $this->setPreviousState();
+        $this->setCurrentY($this->getCurrentY()+1);
+    }
+
+    public function moveLeft()
+    {
+        $this->setPreviousState();
+        $this->setCurrentX($this->getCurrentX()-1);
+    }
+
+    public function moveRight()
+    {
+        $this->setPreviousState();
+        $this->setCurrentX($this->getCurrentX()+1);
+    }
+
+    public function setPreviousState()
+    {
+        $this->setPreviousX($this->getCurrentX());
+        $this->setPreviousY($this->getCurrentY());
+    }
 }
--- game_cli/index.php
+++ game_cli/index.php
@@ -1,53 +1,59 @@
 #!/usr/bin/php -q
 <?php
-/**
- * @param $a
- * @param $b
- * @return string
- */
-function dots($b)
-{
-    $string = '';
-    for ($k = 1; $k <= $b; $k++) {
-        $string .= '.';
-    }
-    return $string;
-}
+require_once 'PersonClass.php';
+//$db = new PDO('sqlite:dbname=testdb;host=127.0.0.1');
 
 ncurses_init();
+ncurses_start_color();
+ncurses_init_pair(1, NCURSES_COLOR_BLUE, NCURSES_COLOR_BLACK);
+ncurses_init_pair(2, NCURSES_COLOR_WHITE, NCURSES_COLOR_BLACK);
 $fullscreen = ncurses_newwin(0, 0, 0, 0);
-    ncurses_clear();
-    ncurses_refresh($fullscreen);
-    ncurses_getmaxyx($fullscreen, $fullscreenHeight, $fullscreenWidth);
-    $frame = ncurses_newwin($fullscreenHeight - 4, $fullscreenWidth - 2, 1, 1);
-    ncurses_wborder($frame, 0, 0, 0, 0, 0, 0, 0, 0);
-    ncurses_wrefresh($frame);
-
-    $field = ncurses_newwin($fullscreenHeight - 6, $fullscreenWidth - 4, 2, 2);
-    ncurses_getmaxyx($field, $fieldHeight, $fieldWidth);
-    $fieldDots = dots($fieldWidth*$fieldHeight);
-    ncurses_mvwaddstr($field, 0, 0, $fieldDots);
-    ncurses_wrefresh($field);
+ncurses_clear();
+ncurses_refresh($fullscreen);
+ncurses_getmaxyx($fullscreen, $fullscreenHeight, $fullscreenWidth);
+$frame = ncurses_newwin($fullscreenHeight - 4, $fullscreenWidth - 2, 1, 1);
+ncurses_wborder($frame, 0, 0, 0, 0, 0, 0, 0, 0);
+ncurses_wrefresh($frame);
+$field = ncurses_newwin($fullscreenHeight - 6, $fullscreenWidth - 4, 2, 2);
+ncurses_getmaxyx($field, $fieldHeight, $fieldWidth);
+$fieldDots = str_repeat('.', $fieldWidth * $fieldHeight);
+ncurses_mvwaddstr($field, 0, 0, $fieldDots);
+ncurses_wrefresh($field);
+$hero = new Person();
+$hero->setCurrentX(rand(0, $fieldWidth));
+$hero->setCurrentY(rand(0, $fieldHeight));
+$hero->setPreviousState();
+$hero->setSymbol('@');
 while (true) {
-    //$db = new PDO('sqlite:dbname=testdb;host=127.0.0.1');
+    ncurses_mvwaddstr($frame, 0, 0, $hero->getCurrentY().'-'.$hero->getCurrentX().'--'.$hero->getPreviousY().'-'.$hero->getPreviousX());
+    ncurses_wrefresh($frame);
+    ncurses_move($hero->getCurrentY(), $hero->getCurrentX());
+//ncurses_delch();
+    ncurses_color_set(1);
+    ncurses_addch(ord($hero->getSymbol()));
+
     $pressed = ncurses_getch();
     if ($pressed == 27) {
         break;
-    } /* else {
+    } else {
         switch ($pressed) {
             case 258:
-                $year--;
+                $hero->moveDown();
                 break;
             case 259:
-                $year++;
+                $hero->moveUp();
                 break;
             case 260:
-                $month--;
+                $hero->moveLeft();
                 break;
             case 261:
-                $month++;
+                $hero->moveRight();
                 break;
         }
+        ncurses_move($hero->getPreviousY(), $hero->getPreviousX());
+        ncurses_color_set(2);
+        ncurses_addch(ord('.'));
+        /*
         if ($month >= 13) {
             $month = 1;
             $year++;
@@ -55,7 +61,8 @@ while (true) {
             $month = 12;
             $year--;
         }
+        */
     }
- */
+
 }
 ncurses_end();
